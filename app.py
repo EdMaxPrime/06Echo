33b12e17cefe53b0b1afcec4d4431368ddd13f59
Max Z <max.zlotskiy@gmail.com>
2017-10-02 19:51:32 -0400

    added route for echo, fixed typos

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template("form.html")
+
+@app.route('/welcome', methods=['GET', 'POST'])
+def tracker():
+    print request.method
+    print request.args
+    return render_template("welcome.html", name = request.args['motto'], query = request.args, method = request.method)
 
 if __name__ == "__main__":
     app.debug = True
