0df1974b67362065a3050906508b34100b4d76c1
Max Z <max.zlotskiy@gmail.com>
2017-10-02 19:52:43 -0400

    removed print statements

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ def root():
 
 @app.route('/welcome', methods=['GET', 'POST'])
 def tracker():
-    print request.method
-    print request.args
     return render_template("welcome.html", name = request.args['motto'], query = request.args, method = request.method)
 
 if __name__ == "__main__":
